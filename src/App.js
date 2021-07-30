ffde985626e0e4088343cf9f7c27013328075797
liuguihua <liuguihua@wanxue.cn>
2021-07-30 17:29:55 +0800

    change:引入redux持久化

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 // import { hot } from 'react-hot-loader/root'
 import routes from "@router/index.js";
-import store from "@store";
+import { store, persistor } from "@store";
 import { renderRoutes } from "react-router-config";
 import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>{renderRoutes(routes)}</HashRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>{renderRoutes(routes)}</HashRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist:['geohash','userInfo']
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-export default store;
+export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+export const persistor = persistStore(store)
